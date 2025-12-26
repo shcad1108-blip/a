9950b204d6e1a44846c119baae6b173d4d030bb4
shcad1108-blip <shcad1108@gmail.com>
2025-12-26 19:39:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-inport streamlit as st
+import streamlit as st
 st.set_page_confih(page_title='우리들의 공유 탈력', layout='centered')
 st.title('전시회 공유 달력 & 방명록')
 stwrite('원하는 날짜에 일정을 남겨보세요. 모든  사람과 공유됩니다!')
